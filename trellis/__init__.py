2dafd13a2e8ccc6e803d004eaca2e9cc2d115364
nicolas barradeau <nicoptere@gmail.com>
2025-01-10 16:46:11 +0100

    cleanup

--- trellis/__init__.py
+++ trellis/__init__.py
@@ -19,7 +19,6 @@ else:
         trellis_operator
         # ...
     )
-    from .utils import blop
     from .trellis_operator import ComputeOperator, DiscretizeOperator
 
 import bpy
@@ -100,12 +99,11 @@ def register():
     bpy.utils.register_class(ComputeOperator)
     bpy.utils.register_class(DiscretizeOperator)
     bpy.utils.register_class(TRELLIS)
-    blop(4)
+    
     
 
 def unregister():
     print( 'goodbye')
-    blop(2)
     bpy.utils.unregister_class(ComputeOperator)
     bpy.utils.unregister_class(DiscretizeOperator)
     bpy.utils.unregister_class(TRELLIS)
@@ -113,58 +111,4 @@ def unregister():
 
 if __name__ == "__main__":
     print( "calling main" ) 
-    blop(1)
     register()
-
-
-"""
-class ObjectCursorArray(bpy.types.Operator):
-    bl_idname = "object.cursor_array"
-    bl_label = "Cursor Array"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    # moved assignment from execute() to the body of the class...
-    total: bpy.props.IntProperty(name="Steps", default=2, min=1, max=100)
-
-    def execute(self, context):
-        scene = context.scene
-        cursor = scene.cursor.location
-        obj = context.active_object
-
-        total = self.total
-
-        for i in range(total):
-            obj_new = obj.copy()
-            scene.collection.objects.link(obj_new)
-
-            obj_new.location[0] = obj.location[0] + i
-            obj_new.location[1] = obj.location[1] + i
-            obj_new.location[2] = obj.location[2] + i
-
-        return {'FINISHED'}
-    
-def menu_func(self, context):
-    self.layout.operator(ObjectCursorArray.bl_idname)
-
-def register():
-    print( 'hello')
-    blop()
-    blop()
-    blop()
-    blop()
-    blop()
-    bpy.utils.register_class(ObjectCursorArray)
-    bpy.types.VIEW3D_MT_object.append(menu_func)
-
-def unregister():
-    print( 'goodbye')
-    blop()
-    blop()
-    bpy.utils.unregister_class(ObjectCursorArray)
-    bpy.types.VIEW3D_MT_object.remove(menu_func)
-
-if __name__ == "__main__":
-    print( "calling main" ) 
-    blop(1)
-    register()
-"""
