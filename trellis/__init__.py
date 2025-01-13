10d25c33ce1f24e7a010ea02f7c60b2d8a14705e
nicolas barradeau <nicoptere@gmail.com>
2025-01-13 22:02:01 +0100

    improved UI panel layout

--- trellis/__init__.py
+++ trellis/__init__.py
@@ -26,16 +26,6 @@ import bpy
 from bpy.types import WindowManager, Scene, Image, Object
 from bpy.props import FloatProperty, IntProperty, PointerProperty
 
-# sparse & SLAT 
-WindowManager.sparse_steps = IntProperty(name="steps", default=12, min=1, max=100)
-WindowManager.sparse_strength = FloatProperty(name="strength", default=7.5, min=0, max=50, step=0.1)
-
-WindowManager.slat_steps = IntProperty(name="steps", default=12, min=1, max=100)
-WindowManager.slat_strength = FloatProperty(name="strength", default=3, min=0, max=50, step=0.1)
-
-# GLB optimization settings
-WindowManager.simplify = FloatProperty(name="simplfication", default=0.95, min=0, max=1, step=0.01)
-WindowManager.texture_size = IntProperty(name="texture size", default=1024, min=64, max=8192, step=1)
 
 class TRELLIS(bpy.types.Panel):
     
@@ -45,6 +35,13 @@ class TRELLIS(bpy.types.Panel):
     bl_region_type = "UI"
     bl_category = "TRELLIS"
 
+    def checkGizmo(self, context):
+        gizmo = bpy.context.view_layer.objects.active
+        if hasattr(gizmo, "data"):
+            if hasattr(gizmo.data, "filepath" ):
+                return gizmo
+        return None
+    
     def draw(self, context):
         
         layout = self.layout
@@ -82,64 +79,51 @@ class TRELLIS(bpy.types.Panel):
         row = box.row(align=True)
         row.operator("object.discretize")
 
-
-# Empty image panel 
-class ImagePanel(bpy.types.Panel):
-    bl_idname = "OBJECT_PT_process"
-    bl_label = "TRELLIS"
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
-    # bl_options = {'DEFAULT_CLOSED'}
-    bl_context = "data"
-
-    @classmethod
-    def poll(cls, context):
-        return (context.object is not None)
-
-    def checkGizmo(self, context):
-        gizmo = bpy.context.view_layer.objects.active
-        if hasattr(gizmo, "data"):
-            if hasattr(gizmo.data, "filepath" ):
-                return gizmo
-        return None
-
-    def draw_header(self, context):
-        layout = self.layout
-        layout.label(text="Panel")
-
-    def draw(self, context):
-        
-        layout = self.layout
+        # check if user clicked on an Empty object (a drag dropped image)
         gizmo = self.checkGizmo(context) 
         if gizmo is not None:
-            
             context.window_manager.gizmo  = gizmo
             context.window_manager.image  = gizmo.data
 
 
-            # box = layout.box()
-            # image_path = context.window_manager.image.filepath
-            # box.label(text="image: " + image_path)
-            # box.operator("object.compute")
-            # box.operator("object.discretize")
-            
 
 def register():
+    # declare some variables
     WindowManager.image = PointerProperty(name="image", type=Image)
     WindowManager.gizmo = PointerProperty(name="gizmo", type=Object)
+
+    # sparse & SLAT settings
+    WindowManager.sparse_steps = IntProperty(name="steps", default=12, min=1, max=100)
+    WindowManager.sparse_strength = FloatProperty(name="strength", default=7.5, min=0, max=50, step=0.1)
+
+    WindowManager.slat_steps = IntProperty(name="steps", default=12, min=1, max=100)
+    WindowManager.slat_strength = FloatProperty(name="strength", default=3, min=0, max=50, step=0.1)
+
+    # GLB optimization settings
+    WindowManager.simplify = FloatProperty(name="simplfication", default=0.95, min=0, max=1, step=0.01)
+    WindowManager.texture_size = IntProperty(name="texture size", default=1024, min=64, max=8192, step=1)
+
     bpy.utils.register_class(ComputeOperator)
     bpy.utils.register_class(DiscretizeOperator)
     bpy.utils.register_class(TRELLIS)
-    bpy.utils.register_class(ImagePanel)
     
 
 def unregister():
+
+    # dispose
+    del WindowManager.image 
+    del WindowManager.gizmo 
+    del WindowManager.sparse_steps
+    del WindowManager.sparse_strength
+    del WindowManager.slat_steps
+    del WindowManager.slat_strength
+    del WindowManager.simplify
+    del WindowManager.texture_size
+
+
     bpy.utils.unregister_class(ComputeOperator)
     bpy.utils.unregister_class(DiscretizeOperator)
     bpy.utils.unregister_class(TRELLIS)
-    bpy.utils.unregister_class(ImagePanel)
-    del WindowManager.image 
-    del WindowManager.gizmo 
 
 if __name__ == "__main__":
     print( "calling TRELLIS main" ) 
